21682ae1634969da21318a487a6a1958f3c804e0
anima-regem <vichukartha@gmail.com>
2023-11-01 18:44:12 +0530

    Coming soon

--- src/components/GetTicket/GetTicket.jsx
+++ src/components/GetTicket/GetTicket.jsx
@@ -2,7 +2,7 @@ import ticket from '../../assets/images/comingsoon.svg';
 const GetTicket = () => {
     return (
         <div id="get-ticket" className="w-full flex p-12">
-            <a href="https://www.yepdesk.com/malabar-hub-meet-2023" className='m-auto'>
+            <a href="#" className='m-auto'>
                 <img src={ticket} alt="" className='m-auto ' />
             </a>
 
