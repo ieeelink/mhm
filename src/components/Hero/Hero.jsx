ea885613e3940b5f2881a94881b95ac1fe8518b1
anima-regem <vichukartha@gmail.com>
2023-11-02 17:36:01 +0530

    adds ticketing

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -26,9 +26,9 @@ const Hero = () => {
                         </div>
                         <a className="btn btn-primary" href="/#get-ticket">
                             <p className=''>
-                                Registration Starts 
+                                Get Early Bird
                                 <br/>
-                                Soon
+                                Tickets
                             </p>
                         </a>
                     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
             <div className="navbar-end">
                 <a className="btn btn-diabled" href='/#get-ticket'>
                     <p className='text-xs'>
-                        Tickets Out Soon
+                        Tickets Out Now!
                     </p>
                 </a>
             </div>
