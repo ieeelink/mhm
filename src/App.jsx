c78a9413a1e184baa1570c673ed67603ca9a1fb9
Nandana Pradeep <nandanapradeep1299@gmail.com>
2023-11-12 15:49:50 +0530

    New component Posteers added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
-import React from 'react';
 
+import React from 'react';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
